db03b60a68e08e4b1e80871bf7e997067d68de44
Cris Mihalache <me@f3rno.com>
2020-06-28 02:08:15 +0700

    feature: add CLI yargs wrapper

--- index.js
+++ index.js
@@ -1,55 +1,100 @@
 #!/usr/bin/env node
 
 const path = require('path')
+const yArgs = require('yargs')
 const colors = require('colors')
-const { createServer, getLogger, getConfig } = require('./lib')
+const { Signale } = require('signale')
+const signaleTypes = require('signale/types')
+const updateNotifier = require('update-notifier')
+const _uniq = require('lodash/uniq')
+const _values = require('lodash/values')
+const _includes = require('lodash/includes')
+const { getConfig, commands } = require('./lib')
+const serveCommand = require('./lib/commands/serve')
+const manifest = require('./package.json')
 
-const DEFAULT_PORT = 8960
-const DEFAULT_HOST = 'localhost'
+updateNotifier({ pkg: manifest }).notify()
 
-const basePath = process.cwd()
-const host = DEFAULT_HOST
-const port = DEFAULT_PORT
+const logLevels = _uniq(_values(signaleTypes).map(({ logLevel }) => logLevel))
+const {
+  describe: serveDescribe,
+  builder: serveBuilder,
+  handler: serveHandler
+} = serveCommand
 
-const l = getLogger('cli')
-
-getConfig(basePath).then((config) => {
-  const { state } = config
-  const { md, configPath, template } = state
-
-  if (configPath) {
-    l.success('loaded user config from %s', colors.bgGreen.black(configPath))
-  } else {
-    l.info('using default config')
-  }
-
-  md.pluginNames.forEach((name) => {
-    l.info('using md plugin %s', colors.yellow(name))
+const y = yArgs
+  .scriptName('sermit')
+  .usage('$0 [command] [options]', serveDescribe, serveBuilder, serveHandler)
+  .option('log-level', {
+    describe: 'Log level, increase to debug',
+    default: 'error',
+    choices: logLevels,
+    type: 'string',
+    alias: 'l',
+    global: true
   })
-
-  l.info('using template %s', colors.cyan(template.name))
-
-  // const { state: _, ...logConfig } = config
-
-  // JSON
-  //   .stringify(logConfig, null, 2)
-  //   .split('\n')
-  //   .forEach(ln => { l.info('config: %s', ln) })
-
-  const server = createServer({
-    config,
-    basePath,
-    assetPath: path.join(__dirname, '/public')
+  .option('path', {
+    describe: 'Root directory',
+    default: process.cwd(),
+    demandOption: true,
+    type: 'string',
+    alias: 'p',
+    global: true
   })
+  .middleware((argv) => {
+    const { 'log-level': logLevel } = argv
+    const l = new Signale({
+      logLevel,
+      scope: 'cli',
+      types: {
+        ...signaleTypes,
+        star: {
+          ...signaleTypes.star,
+          logLevel: 'error'
+        }
+      }
+    })
 
-  server.listen(port)
+    l.info('started [%s]', logLevel)
 
-  l.star(
-    '%s',
-    colors.cyan.underline(
-      `listening at ${colors.bold(`http://${host}:${port}`)}`
-    )
-  )
-}).catch((e) => {
-  l.error('%s', e.stack)
-})
+    argv.l = l
+  })
+  .middleware(async (argv) => {
+    const { _, l, path: rawPath } = argv
+    const cwd = process.cwd()
+    const basePath = rawPath[0] === '.'
+      ? path.join(cwd, rawPath.slice(1))
+      : _includes(rawPath, path.sep)
+        ? rawPath
+        : path.join(cwd, rawPath)
+
+    const [command] = _
+    const config = await getConfig(basePath)
+    const { state } = config
+    const { md, configPath, template } = state
+
+    if (configPath) {
+      l.info('read config from %s', colors.bgGreen.black(configPath))
+    }
+
+    if (command === 'serve' || command === 'render') {
+      l.star('using template %s', colors.cyan(template.name))
+
+      md.pluginNames.forEach((name) => {
+        l.star('using md plugin %s', colors.yellow(name))
+      })
+    }
+
+    // eslint-why save config on args context
+    /* eslint-disable-next-line require-atomic-updates */
+    argv.config = config
+
+    // eslint-why save config on args context
+    /* eslint-disable-next-line require-atomic-updates */
+    argv.path = basePath
+  })
+  .help()
+
+commands.forEach((def) => { y.command(def) })
+
+y.parse()
