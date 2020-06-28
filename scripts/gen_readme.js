a510daad66797fab55ce0312b2f3f783f6bf4975
Cris Mihalache <me@f3rno.com>
2020-06-28 18:04:40 +0700

    chore: update to match other sermit libs, deciding structure

--- scripts/gen_readme.js
+++ scripts/gen_readme.js
@@ -2,26 +2,24 @@ const path = require('path')
 const { promises: fs } = require('fs')
 const _template = require('lodash/template')
 const jsDoc2MD = require('jsdoc-to-markdown')
-const { getLogger } = require('../')
+const { getLogger } = require('serve-markdown-it-lib')
 
 /**
  * @private
  */
 const getPath = p => path.join(__dirname, '../', p)
 
-const TEMPLATE_SRC = '.template.README.md'
+const TEMPLATE_SRC = '.README.md'
 const TEMPLATE_DST = 'README.md'
 const API_SRC = ['*.js', 'lib/**'].map(getPath)
-const USAGE_SRC = ['index.js'].map(getPath)
 
 const l = getLogger('gen-readme')
 
 jsDoc2MD.render({ files: API_SRC }).then(async (api) => {
-  const usage = await jsDoc2MD.render({ files: USAGE_SRC })
   const templatePath = getPath(TEMPLATE_SRC)
   const templateSrc = await fs.readFile(templatePath, 'utf-8')
   const template = _template(templateSrc)
-  const readme = template({ usage, api })
+  const readme = template({ api })
   const dst = getPath(TEMPLATE_DST)
 
   await fs.writeFile(dst, readme)
